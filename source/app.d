e8cd9b5f1015d86eefae33b54a33acaec03dd922
treyzania <treyzania@gmail.com>
2017-05-26 23:01:42 -0400

    Reimplemented the base of artifact definitions.

--- source/app.d
+++ source/app.d
@@ -1,28 +1,3 @@
-/*
- * this file has the main entry point to the CLI // FIXME This shouldn't be right.
- */
-import std.stdio;
-import std.digest.digest : toHexString;
-import blockchain;
-import artifacts;
-
 void main() {
-    ubyte[] sig = [1, 2, 3, 4, 5, 6];
-
-    auto a1 = Artifact(sig, ArtifactHeader([], 0u),
-                       new ReplyArtifact([1,2,3], "Hello, world!"));
-
-    auto a2 = Artifact(sig, ArtifactHeader([], 0u),
-                       new ReplyArtifact([1,2,4], "Hello, world!"));
-
-    auto a3 = Artifact(sig, ArtifactHeader([], 0u),
-                       new ReplyArtifact([1,2,3], "Hello, world!!"));
-
-    protocol_digest sha;
-
-    foreach (Artifact art; [a1, a2, a3]) {
-        sha.start();
-        art.hash(&sha);
-        writeln(sha.finish().toHexString());
-    }
+	
 }
--- source/blockchain.d
+++ source/blockchain.d
@@ -1,95 +1,35 @@
-/*
- * this file holds data definitions for the components of the blockchain
- */
 import std.outbuffer : OutBuffer;
 import std.bitmanip : nativeToBigEndian;
 import std.container.array : Array;
 import std.digest.sha : SHA256;
 
-alias protocol_digest = SHA256;
+alias hash_function = SHA256;
+alias addr = ubyte[32]; // This is the hash size.
+alias signature = ubyte[32]; // This is also the hash size but for different reasons.
+alias timestamp = ulong;
 
-/*
- * I don't know what the naming conventions for D are.
- * I'm using _t for aliases, but is this good?
- */
-alias credit_t = uint;
-alias sign_t = ubyte[]; // SHA-256 -> 32 bytes
+struct ArtifactEntry {
+    signature sig;
+    Artifact artifact;
+}
 
-/*
- * what are addresses? are they just hashes?
- * why don't we use signatures as addresses, since they're basically
- * the same thing? something to think about...
- */
-alias addr_t = ubyte[];
-
-/*
- * an artifact created by some identity in the blockchain
- */
 struct Artifact {
-
-    sign_t signature;
-    ArtifactHeader header;
-    ArtifactData data;
-
-    /* updates the digest with this data */
-    void hash(protocol_digest* dig) {
-        this.header.hash(dig);
-        this.data.hash(dig);
-    }
-
+    ushort type;
+    timestamp time;
+    addr authorDeclAddr;
+    ArtifactContents contents;
 }
 
-/*
- * the header information contained in all artifacts
- */
-struct ArtifactHeade {
-
-    addr_t author;
-    ulong timestamp; // ulong = 8 bytes
-
-    /* updates the digest with this data */
-    void hash(protocol_digest* dig) {
-        dig.put(this.author);
-        dig.put(nativeToBigEndian(this.timestamp));
-    }
-
+interface ArtifactContents {
+    ubyte[] getBytes();
 }
 
-/*
- * an interface for any types of artifacts
- */
-interface ArtifactData
-{
-    /* returns the lowest version of the protocol that supports this kind of artifact */
-    uint getVersionNum();
+class UnknownArtifact : ArtifactContents {
 
-    /* returns the credit cost the author payed to publish this artifact */
-    credit_t getCost();
+    ubyte[] raw;
 
-    /* updates the digest with this data */
-    void hash(protocol_digest* dig);
-}
-
-
-/*
- * a PoS block in the chain
- */
-struct Block {
-
-    sign_t signature;
-    addr_t author;
-    ulong timestamp;
-    addr_t prev;
-    Array!(addr_t) artifacts;
-
-    /* updates the digest with this data */
-    void hash(protocol_digest* dig) {
-        dig.put(this.author);
-        dig.put(nativeToBigEndian(this.timestamp));
-        dig.put(this.prev);
-        foreach(addr_t a; artifacts) {
-            dig.put(a);
-        }
+    ubyte[] getBytes() {
+        return raw;
     }
 
 }
